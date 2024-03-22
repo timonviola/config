bfb63ecab103ed256117cdc997497cd4822d6ea7
Timon Viola <44016238+timonviola@users.noreply.github.com>
2024-03-22 13:31:30 +0100

    feat(nvim): remove print lines

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,1 @@
 require("timon")
-print("initlua")
--- nvim/lua/timon/packer.lua
+++ nvim/lua/timon/packer.lua
@@ -2,7 +2,6 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-print("packer")
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
