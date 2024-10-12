cc9b9784c5bb46b20ee0325e6b25b3e50a7144ee
Timon Viola <44016238+timonviola@users.noreply.github.com>
2024-10-12 16:35:56 +0200

    feat(nvim): add copilot and copilot chat

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -53,5 +53,24 @@ return {
     -- notifications
     'rcarriga/nvim-notify',
     'timonviola/easy-to-change.nvim',
+    {
+        'zbirenbaum/copilot.lua',
+        config = function()
+            require('copilot').setup {}
+        end,
+    },
+    {
+      "CopilotC-Nvim/CopilotChat.nvim",
+      branch = "canary",
+      dependencies = {
+        { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+        { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+      },
+      opts = {
+        debug = true, -- Enable debugging
+        -- See Configuration section for rest
+      },
+      -- See Commands section for default commands if you want to lazy load on them
+    },
 }
 
