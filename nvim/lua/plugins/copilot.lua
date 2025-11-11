44bf1ae57b1d3f856bbacaaeb629f4919a0323a0
Timon <44016238+timonviola@users.noreply.github.com>
2025-11-11 16:22:20 +0100

    feat(nvim): add copilot and rust plugins

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,3 +1,42 @@
 return {
-    "github/copilot.vim",
+    -- all config options: https://github.com/CopilotC-Nvim/CopilotChat.nvim/blob/main/lua/CopilotChat/config.lua
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+            { "github/copilot.vim" },
+            { "nvim-lua/plenary.nvim", branch = "master" },
+        },
+        build = "make tiktoken",
+        opts = {
+            -- See Configuration section for options
+            prompts = {
+                MyCustomPrompt = {
+                    prompt = 'Explain how it works.',
+                    system_prompt = 'You are very good at explaining stuff',
+                    mapping = '<leader>zmc',
+                    description = 'My custom prompt description',
+                },
+                Yarrr = {
+                    system_prompt = 'You are fascinated by pirates, so please respond in pirate speak.',
+                },
+                NiceInstructions = {
+                    system_prompt = 'You are a nice coding tutor, so please respond in a friendly and helpful manner.',
+                }
+            },
+            window = {
+                layout = 'vertical',
+                border = 'rounded', -- 'single', 'double', 'rounded', 'solid'
+                title = '🤖 AI Assistant',
+            },
+
+            headers = {
+                user = '👤 You',
+                assistant = '🤖 Copilot',
+                tool = '🔧 Tool',
+            },
+
+            separator = '━━',
+            auto_fold = true, -- Automatically folds non-assistant messages
+        },
+    },
 }
--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -48,3 +48,4 @@ return {
 --vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#717aa3' })
 --vim.api.nvim_set_hl(0, 'LineNr', { fg='yellow' })
 --vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#717aa3' })
+--# vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { link = "DiagnosticUnnecessary" }) when unused code is too dark
