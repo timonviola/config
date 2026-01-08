0905e541a9bb549b7079bca6cef9d9638cd45021
Timon <44016238+timonviola@users.noreply.github.com>
2026-01-08 19:06:06 +0100

    feat(nvim): add amp plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -74,5 +74,11 @@ return {
                 }
             }
         end
+    },
+    {
+        "sourcegraph/amp.nvim",
+        branch = "main",
+        lazy = false,
+        opts = { auto_start = true, log_level = "info" },
     }
 }
